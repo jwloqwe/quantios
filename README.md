41e0f11583b38867aea3f5ac3f366475f2a6a287
jwloqwe <chanjunwei2000@gmail.com>
2025-10-30 00:26:44 +0800

    main - Initialize Project, Added Playwright Test Cases and Postman Collection.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# quantios
-Playwright Automation &amp; Postman
+# Quantios
+
+- This project will be writing out Automated Test Cases using the Web link from Quantios
+- Test Automation Tool that will be used is PlayWright
+- The API testing tool that will be used is Postman.
---
+++ e2e/Login.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Login Flow', () => {
+    test('Register then Login', async ({ page }) => {
+        // Access the site and make sure that the Title of the Page is correct
+        await page.goto('');
+        expect(page).toHaveTitle('Login Page');
+    
+        // Look for the link with the 'Create New User' label
+        await page.getByRole('link', { name: 'Create new user' }).click();
+
+        // Ensure that the page is present
+        expect(page).toHaveURL('\/register');
+        await page.waitForSelector('#registerForm');
+        
+        // Expect that the Button is Disabled
+        const createAcc = page.getByRole('button', { name: "Create Account" });
+        await expect(createAcc).toBeDisabled();
+        
+        // Look for the Username and Passoword then fill the credentials using env variables.
+        const randomNumber = Math.floor(Math.random() * 100);
+        const username = process.env.Q_USERNAME! + randomNumber +'@gmail.com';
+        const password = process.env.Q_PASSWORD!;
+        await page.getByLabel('Username').fill(username);
+        await page.getByLabel('Password').fill(password);
+        await (createAcc).click({ force: true });
+
+        // Login Into Account
+        await page.waitForSelector('#loginForm');
+        const loginBtn = page.getByRole('button', { name: "Login" });
+        await page.getByPlaceholder('Email Address').fill(username);
+        await page.getByPlaceholder('Password').fill(password);
+        await loginBtn.click({ force: true });
+        
+        // Verify Dashboard is Present
+        await page.waitForSelector('.dashboard');
+        await expect(page.locator('h1')).toHaveText('Stock Market Dashboard');
+    });
+});
